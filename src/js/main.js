8f55f2f9f261af7aab5725a38b0a0af76caed95a
samarthyasurve17 <samarthyasurve17@gmail.com>
2024-10-06 09:22:58 +0000

    half website complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,43 @@
-// to targetthe input element
+import data from "./data.json";
+// DOM TARGETING
+const cardsrow = document.querySelector("#cards-row");
 const inputEl = document.querySelector("input");
+
+// const paragraph = document.createElement("p");
+// paragraph.textContent = "our class";
+// cardsrow.appendChild(paragraph);
+
+console.log(cardsrow);
+
+for (let pokObj of data) {
+  const div =document.createElement("div");
+  div.classList.add("col");
+  div.innerHTML =` 
+  <div class="card">
+<img src="${pokObj.image}" class="card-img-top" alt="..." />
+<div class="card-body">
+    <h5 class="card-title">${pokObj.name}</h5>
+    <p class="card-text">
+   ${pokObj.description}
+    </p>
+</div>
+</div>
+</div>
+
+  `;
+  cardsrow.appendChild(div);
+}
+
+// console.log(data);
+// // to targetthe input element
+// const inputEl = document.querySelector("input");
+
 // log the targeted element
 // inputEl.focus();
 document.addEventListener("keypress", function (event) {
   if (event.key === "/") {
     // it will not type "/" when we press "/"
-    event.preventDefault();  
+    event.preventDefault();
     inputEl.focus();
   }
 });
